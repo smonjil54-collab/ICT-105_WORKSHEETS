ace244bca00b86f4ee702085609b52c955dd3b54
shahriar <smonjil54@gmail.com>
2026-09-07 22:45:48 +0800

    Add variable declaration and types - Exercise 1 complete

---
+++ worksheet1_session2.py
@@ -0,0 +1,14 @@
+# Variable Declaration
+a = 15
+b = 12
+
+print("Value of a:", a)
+print("Value of b:", b)
+
+# Step 2: Check Types
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+
+# Step 3: Addition
+result = a + b
+print("Addition:", a, "+", b, "=", result)
